7fba4852690be49f092385a69395259d699f7ff4
drjhatka <bishawjit12@gmail.com>
2024-05-23 04:40:58 -0400

    wishlist done

---
+++ src/components/BookDetailCard.jsx
@@ -0,0 +1,49 @@
+import { MdOutlineEditLocation } from "react-icons/md";
+import {  SiPowerpages } from "react-icons/si";
+import { FiUsers } from "react-icons/fi";
+import{Link} from 'react-router-dom'
+function BookDetailCard({book}) {
+    const {image,bookName}=book
+    return (
+        <div className='flex mb-4 bg-base-300 gap-10 border-2 px-2 py-4 rounded-lg'>
+            <div><img src={'.'+image} className="max-w-40 min-h-56" alt="" /></div>
+            <div className="flex flex-col gap-5">
+                <div className="font-semibold text-2xl">{book.bookName}</div>
+                <div>By: {book.author}</div>
+                <div className="flex gap-6">
+                    <h1 className="font-bold">Tags</h1>
+                    {
+                        book.tags.map(tag=>{
+                            return<>
+                                <div key={Math.random()*100} className="text-white font-semibold py-4 px-3 badge badge-success gap-2">
+                                    # {tag}
+                                </div>
+                                </>
+                            
+                        })
+                    }
+                    <h1 className="font-semibold flex items-center justify-center gap-4"><MdOutlineEditLocation className="text-lg text-red-700" /> Year of Publishing: {book.yearOfPublishing}</h1>
+                </div>
+                <div className="flex gap-10 items-center ga">
+                <FiUsers/ ><h1>Publisher:</h1> <h1>{book.publisher}</h1><div className="flex items-center justify-center gap-3 font-semibold"><SiPowerpages/><h1> Page : {book.totalPages}</h1></div>
+                </div>
+                <div className="flex items-center gap-6">
+                    <div className="badge badge-warning gap-2">
+                        Category: {book.category}
+                    </div>
+                    <div className="badge badge-warning gap-2">
+                        Rating: {book.rating}
+                    </div>
+                    <div className="flex">
+                    <Link className="btn btn-success text-white" to={'/book-details/'+book.bookId} >
+                        View Details
+                    </Link>
+                </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default BookDetailCard
--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
+import BookDetailCard from './BookDetailCard'
+import BookWishListCard from './BookWishListCard'
 
 function BookList() {
-    const readlist = JSON.parse(localStorage.getItem('readlist'))
-    console.log(readlist)
-    const wishlist = JSON.parse(localStorage.getItem('wishlist'))
+
+    const [read, setRead] = useState([])
+    const [wish, setWish] = useState([])
+    useEffect(()=>{
+        setRead(JSON.parse(localStorage.getItem('readlist')))
+        setWish(JSON.parse(localStorage.getItem('wishlist')))
+
+    },[])
+    console.log(read)
     return (
-        <div>
-                <div role="tablist" className="tabs tabs-lifted">
-                    <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 1" />
+        <div className='mt-5'>
+            <div className='flex justify-center rounded-lg items-center text-3xl bg-[#ff3333] py-6 text-white mb-4 font-bold'>
+                <h1>Books</h1>
+            </div>
+            <div className='flex justify-center rounded-lg items-center'>
+                
+            </div>
+            <div role="tablist" className="tabs tabs-lifted">
+               
+                <input type="radio"  defaultChecked={true} name="my_tabs_2" role="tab" className="tab" aria-label="Book Read List" checked />
+                <div role="tabpanel" className=" tab-content bg-base-100 border-base-300 rounded-box p-6">
+                    {
+                    read ? read.map(book=>{ 
+                        return <BookDetailCard key={book.bookId} book={book}></BookDetailCard>
+                        }):[]
+                    }
+                </div>
+                <input type="radio" defaultChecked={true}  name="my_tabs_2" role="tab" className="tab" aria-label="Books Wishlist" />
                     <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-                        { readlist!=null &&  readlist.map(book=> <li key={Math.random()*100}> {book.bookName} </li>) }
+                    {
+                        wish ? wish.map(book=>{ 
+                                return <BookWishListCard key={book.bookId} book={book}></BookWishListCard>
+                            }):[]
+                    }
                     </div>
 
-                    <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 2" checked />
-                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-                        { wishlist!=null && wishlist.map(book=> <li key={Math.random()*100}> {book.bookName} </li>) }
-                    </div>
-              </div>
+                
+
+            </div>
 
         </div>
     )
